c76520fab3d0dfcd959d40589f138d3bf2c4616b
rachelsch1 <schwarz.rachel.e@gmail.com>
2021-06-06 15:13:32 -0400

    initial solution

--- src/main/java/ex16/base/ex16.java
+++ src/main/java/ex16/base/ex16.java
@@ -1,4 +1,15 @@
 package ex16.base;
 
+import java.util.Scanner;
+
 public class ex16 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is your age? ");
+        String str1 = input.nextLine();
+        int age = Integer.parseInt(str1);
+
+        System.out.println((age >= 16) ? "You are old enough to legally drive." : "You are not old enough to legally drive.");
+    }
 }
